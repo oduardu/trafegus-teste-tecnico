acc73caf04f56739dcc9e49a2361a9507349b334
Eduardo Pazzini Zancanaro <eduardo.zancanaro@estudante.uffs.edu.br>
2024-09-12 13:27:40 -0300

    feat: criado entidades veiculo e vinculo

---
+++ trafegus-system/module/Application/src/Application/Model/BondVehiclesAndDrivers.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Application\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="driver_vehicle")
+ */
+class BondVehiclesAndDrivers
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string", length=11, nullable=false, unique=true)
+     */
+    protected $driver_cpf;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true, unique=true)
+     */
+    protected $vehicle_placa;
+
+    public function getDriverCpf()
+    {
+        return $this->driver_cpf;
+    }
+
+    public function getVehiclePlaca()
+    {
+        return $this->vehicle_placa;
+    }
+    public function setDriverCpf($cpf)
+    {
+        $this->driver_cpf = $cpf;
+    }
+
+    public function setVehiclePlaca($placa)
+    {
+        $this->vehicle_placa = $placa;
+    }
+
+}
---
+++ trafegus-system/module/Application/src/Application/Model/Vehicles.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Application\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="vehicles")
+ */
+class Vehicles
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string", length=7, nullable=false, unique=true)
+     */
+    protected $placa;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true, unique=true)
+     */
+    protected $renavam;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=false)
+     */
+    protected $modelo;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=false)
+     */
+    protected $marca;
+
+    /**
+     * @ORM\Column(type="int", nullable=false)
+     */
+    protected $ano;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=false)
+     */
+    protected $cor;
+
+    public function getPlaca()
+    {
+        return $this->placa;
+    }
+
+    public function getRenavam()
+    {
+        return $this->renavam;
+    }
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    public function getAno()
+    {
+        return $this->ano;
+    }
+
+    public function getCor()
+    {
+        return $this->cor;
+    }
+
+    public function setPlaca($placa)
+    {
+        $this->placa = $placa;
+    }
+
+    public function setRenavam($renavam)
+    {
+        $this->renavam = $renavam;
+    }
+
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+    }
+
+    public function setAno($ano)
+    {
+        $this->ano = $ano;
+    }
+
+    public function setCor($cor)
+    {
+        $this->cor = $cor;
+    }
+
+}
